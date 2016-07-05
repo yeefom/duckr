065df29e21931dc0f4456fee5ff1537aa8eb7051
Yifeng Wang <yeefom@gmail.com>
2016-07-04 20:07:17 -0700

    Use ES6 Class style

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {container, innerContainer} from './styles.css';
 import {Navigation} from 'components';
 
-const MainContainer = React.createClass({
+class MainContainer extends Component {
   render() {
     return (
       <div className={container}>
-        <Navigation isAuthed={true}/>
+        <Navigation isAuthed={true} />
         <div className={innerContainer}>
           {this.props.children}
         </div>
       </div>
     );
   }
-});
+}
 
 export default MainContainer;
