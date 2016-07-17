2afc69397aea516a7602a90ed22d1833cc61fdf0
Yifeng Wang <yeefom@gmail.com>
2016-07-17 15:40:36 -0700

    Connect app to redux

--- app/index.js
+++ app/index.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import routes from './config/routes';
+import users from './redux/modules/users';
+
+const store = createStore(users);
 
 ReactDOM.render(
-  routes,
+  <Provider store={store}>
+    {routes}
+  </Provider>,
   document.getElementById('app')
 );
-
