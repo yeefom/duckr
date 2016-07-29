25957e8ed483a077d3c24e297a705088395b3834
Yifeng Wang <yeefom@gmail.com>
2016-07-29 14:19:59 -0700

    Connect MainContainer to redux

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,12 +1,13 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {container, innerContainer} from './styles.css';
+import {connect} from 'react-redux';
 import {Navigation} from 'components';
 
 class MainContainer extends Component {
   render() {
     return (
       <div className={container}>
-        <Navigation isAuthed={false} />
+        <Navigation isAuthed={this.props.isAuthed} />
         <div className={innerContainer}>
           {this.props.children}
         </div>
@@ -15,4 +16,10 @@ class MainContainer extends Component {
   }
 }
 
-export default MainContainer;
+MainContainer.propTypes = {
+  isAuthed: PropTypes.bool.isRequired
+};
+
+export default connect(
+  state => ({isAuthed: state.isAuthed})
+)(MainContainer);
