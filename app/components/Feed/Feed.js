43bf87977329fb7a8fa8236ff303fafec1d73cc5
Yifeng Wang <yeefom@gmail.com>
2016-07-29 14:19:41 -0700

    Set up Feed page

---
+++ app/components/Feed/Feed.js
@@ -0,0 +1,8 @@
+import React, {PropTypes} from 'react';
+
+function Feed() {
+  return (
+    <div>Feed!!!</div>
+  );
+}
+export default Feed;
--- app/config/routes.js
+++ app/config/routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Router, hashHistory, IndexRoute, Route} from 'react-router';
-import {MainContainer, HomeContainer, AuthenticateContainer} from 'containers';
+import {MainContainer, HomeContainer, AuthenticateContainer, FeedContainer} from 'containers';
 
 const routes = (
   <Router history={hashHistory}>
     <Router path='/' component={MainContainer}>
       <IndexRoute component={HomeContainer} />
       <Route path='auth' component={AuthenticateContainer} />
+      <Route path='feed' component={FeedContainer} />
     </Router>
   </Router>
 );
