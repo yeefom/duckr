1f7cbe02712a9de656402115954c88e55266836b
Yifeng Wang <yifeng@yeefom.com>
2016-09-10 22:26:01 -0700

    Pass down duck info

--- app/containers/Duck/DuckContainer.js
+++ app/containers/Duck/DuckContainer.js
@@ -1,27 +1,10 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import {Duck} from 'components';
+import * as usersLikesActions from 'redux/modules/usersLikes';
 
 const {object, number, bool, func} = PropTypes;
-
-DuckContainer.propTypes = {
-  duck: object.isRequired,
-  numberOfLikes: number,
-  isLiked: bool.isRequired,
-  hideLikeCount: bool.isRequired,
-  hideReplyBtn: func.isRequired,
-  handleDeleteLike: func.isRequired,
-  addAndHandleLike: func.isRequired
-};
-
-DuckContainer.defaultProps = {
-  hideReplyBtn: false,
-  hideLikeCount: true
-};
-
-DuckContainer.contextTypes = {
-  router: PropTypes.object.isRequired
-};
 
 class DuckContainer extends Component {
   goToProfile(e) {
@@ -43,16 +26,39 @@ class DuckContainer extends Component {
   }
 }
 
+DuckContainer.propTypes = {
+  duck: object.isRequired,
+  numberOfLikes: number,
+  isLiked: bool.isRequired,
+  hideLikeCount: bool.isRequired,
+  hideReplyBtn: bool.isRequired,
+  handleDeleteLike: func.isRequired,
+  addAndHandleLike: func.isRequired
+};
+
+DuckContainer.defaultProps = {
+  hideReplyBtn: false,
+  hideLikeCount: true
+};
+
+DuckContainer.contextTypes = {
+  router: PropTypes.object.isRequired
+};
+
 function mapStateToProps({ducks, likeCount, usersLikes}, props) {
-  const key = props.key;
+  const duckId = props.duckId;
 
   return {
-    duck: ducks[key],
-    numberOfLikes: likeCount[key],
-    isLiked: usersLikes[key],
+    duck: ducks[duckId],
+    numberOfLikes: likeCount[duckId],
+    isLiked: usersLikes[duckId] === true,
     hideLikeCount: props.hideReplyBtn,
     hideReplyBtn: props.hideReplyBtn
   };
 }
 
-export default connect(mapStateToProps)(DuckContainer);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(usersLikesActions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DuckContainer);
