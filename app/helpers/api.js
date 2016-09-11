629d7b260b8c8c0c10da9c81ba7264f692208bc2
Yifeng Wang <yifeng@yeefom.com>
2016-09-10 22:26:27 -0700

    Update like to firebase

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -29,11 +29,32 @@ export function saveDuck(duck) {
 }
 
 export function listenToFeed(cb, errorCb) {
-  // 'value', get change response as a whole
+  // on, listen to change
+  // value, get change response as a whole
   // https://www.firebase.com/docs/web/api/query/on.html
   ref.child('ducks').on('value', snapshot => {
     const feed = snapshot.val() || {};
     const sortedIds = Object.keys(feed).sort((a, b) => feed[b].timestamp - feed[a].timestamp);
     cb(feed, sortedIds);
   }, errorCb);
+}
+
+export function fetchUsersLikes(uid) {
+  return ref.child(`usersLikes/${uid}`).once('value').then(snapshot => snapshot.val() || {});
+}
+
+export function saveToUsersLikes(uid, duckId) {
+  return ref.child(`usersLikes/${uid}/${duckId}`).set(true);
+}
+
+export function deleteFromUsersLikes(uid, duckId) {
+  return ref.child(`usersLikes/${uid}/${duckId}`).set(null);
+}
+
+export function incrementNumberOfLikes(duckId) {
+  return ref.child(`likeCount/${duckId}`).transaction((currentValue = 0) => currentValue + 1);
+}
+
+export function decrementNumberOfLikes(duckId) {
+  return ref.child(`likeCount/${duckId}`).transaction((currentValue = 0) => currentValue - 1);
 }
